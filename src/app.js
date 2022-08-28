d6a6e4a6d1a09cea1f0035b83edd4911a451a98e
emvalencaf <edsonmvf@gmail.com>
2022-08-28 19:43:35 -0300

    tentativa de arrumar o erro de cors
    usei o cors do NodeJS como um middleware para resolver o problema de cross da API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
 
 const certificadosRoutes = require('./routes/certificados.routes')
 
